7230a50c3708cc0a65825f0a9e7503bd4c95ccc1
Hugo Castro Costa <hg.tp.castro@gmail.com>
2021-01-28 08:54:17 -0300

    476..478 - Classe carro

--- dart-oo/modelo/data.dart
+++ dart-oo/modelo/data.dart
@@ -26,28 +26,3 @@ class Data {
     return obterFormatada();
   }
 }
-
-main() {
-  var dataAniversario = new Data(22, 02, 1980);
-
-  var dataCompra = new Data(01, 01, 2021);
-  dataCompra.dia = 31;
-  dataCompra.mes = 12;
-  dataCompra.ano = 2021;
-
-  var da = dataAniversario.obterFormatada();
-
-  print('A data de aniversário é: $da');
-
-  print('A data da compra é: ${dataCompra.obterFormatada()}');
-
-  print(dataCompra);
-  print(dataCompra.toString());
-
-  print(Data.com(ano: 2022));
-
-  var dataFinal = Data.com(dia: 12, mes: 7, ano: 2024);
-  print(dataFinal);
-
-  print(Data.ultimoDiaDoAno(2023));
-}
