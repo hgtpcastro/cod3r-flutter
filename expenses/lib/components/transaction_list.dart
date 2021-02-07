ba451e52ea4252136aa5bdcde6f56273105efefa
Hugo Castro Costa <hg.tp.castro@gmail.com>
2021-02-07 08:05:58 -0300

    105 - Fontes personalizadas e Theme

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -45,10 +45,7 @@ class TransactionList extends StatelessWidget {
                   children: [
                     Text(
                       transaction.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
+                      style: Theme.of(context).textTheme.headline6,
                     ),
                     Text(
                       DateFormat('d MMM y').format(transaction.date),
