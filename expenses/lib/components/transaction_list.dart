546f45190ce874c53f0d364dcfb0710390a18836
Hugo Castro Costa <hg.tp.castro@gmail.com>
2021-02-07 12:54:05 -0300

    123 - Ajustando altura da lista

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -17,7 +17,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 270,
       child: transactions.isEmpty
           ? Column(
               children: [
